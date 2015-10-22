cde35bc62c2a9bb3f9bea2be0c62626252c5c084
Bryan Jimenez <bsjc96@gmail.com>
2015-10-22 00:16:18 -0600

    Hola mundo con mpi

---
+++ mpi_hola.c
@@ -0,0 +1,13 @@
+/* C Example */ 
+#include <mpi.h> 
+#include <stdio.h> 
+int main (int argc, char* argv[]) 
+{ int rank, size; 
+MPI_Init (&argc, &argv);
+ /* starts MPI */ 
+MPI_Comm_rank (MPI_COMM_WORLD, &rank); /* get current process id */ 
+MPI_Comm_size (MPI_COMM_WORLD, &size); /* get number of processes */ 
+printf( "Hola soy el proveso %d de %d\n", rank, size ); 
+MPI_Finalize(); 
+return 0; 
+}
